0ea75b5f6b739bf06b7002a82d990d66df013a7b
TheMartonfi <carlmartonfi@hotmail.com>
2020-11-12 19:43:44 -0700

    Add icons to UploadFile component

--- src/components/UploadFile/Show.jsx
+++ src/components/UploadFile/Show.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import UploadButton from "./UploadButton";
+import { ReactComponent as DocumentLeft } from "../../assets/Document-Left.svg";
+import { ReactComponent as SpreadsheetCenter } from "../../assets/Spreadsheet-Center.svg";
+import { ReactComponent as FolderRight } from "../../assets/Folder-Right.svg";
 
 const Show = (props) => {
 	return (
 		<div>
+			<DocumentLeft />
+			<SpreadsheetCenter />
+			<FolderRight />
 			<h1>Drag and drop</h1>
 			<p>your document here or</p>
 			{/* Hide input file and make this button click input file so I can customize text */}
